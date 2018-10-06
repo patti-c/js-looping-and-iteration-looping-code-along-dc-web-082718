6915b7c12543755b6bafb751dc5c1d0246489da1
patti-c <patricia.carter@flatironschool.com>
2018-10-06 13:11:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function printBadges(name_array) {
+  for(let i = 0; i < name_array.length; i++) {
+    console.log(`Welcome ${name_array[i]}! You are employee #${i+1}.`);
+  }
+  return name_array
+}
+
+function tailsNeverFails() {
+
+  function coinFlip() {
+    return Math.random() >= 0.5;
+  }
+
+  let tails = 0;
+
+  while(coinFlip()) {
+    tails +=1;
+  }
+
+  return `You got ${tails} tails in a row!`
+
+}
